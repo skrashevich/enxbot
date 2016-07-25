85ddfd0e2214dce9270861472fc0b3a6186e89c4
Sergey <svk@svk.su>
2016-07-25 15:33:40 +0300

    initial commit

---
+++ bot.php
@@ -0,0 +1,245 @@
+<?php
+error_reporting(E_ALL & ~(E_STRICT|E_NOTICE));
+
+define('BOT_TOKEN', '***REMOVED-TOKEN***');
+define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('WEBHOOK_URL', 'https://shtab.svk.su/enbot/bot.php');
+
+include('functions.php');
+
+$settings_all = json_decode(file_get_contents('config'),true);
+
+// if run from console, set or delete webhook
+if (php_sapi_name() == 'cli') {
+  apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
+  die();
+}
+
+$helptext = "Это бот для игры Encounter 
+Основная цель бота - пробитие кодов.
+
+Настройка бота:
+/game domain <domain> - задать домен
+/game login <login> - задать логин движка
+/game pass <pass> - задать пароль движка. Пароль задается в зашифрованном виде!
+/game id <id> - ID игры (из адресной строки!)
+
+/game auth - авторизоваться на движке
+/game print - вывод настроек
+
+/game start - старт бота
+/game stop - остановка бота
+/game delete - удалить все настройки игры в канале
+
+
+/encrypt <пароль> - в личку боту! получить зашифрованный пароль для установки в канале
+
+Игровой процесс:
+
+/level - отобразить текст текущего уровня
+/hints - отобразить подсказки на уровне
+
+Коды пробивать с префиксом & либо #
+Например: &en123
+
+/help или /start - помощь
+
+Автор бота: @skrashevich";
+
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+if(isset($update["message"]))
+{
+	$message=$update["message"];
+	logMessage($message);
+
+    if(isset($message["text"]))
+    {
+        $text=$message["text"];
+		$message_id=$message['message_id'];
+		$chat_id=$message['chat']['id'];
+
+        
+        if(!isset($settings_all[md5($chat_id)]))
+            $settings_all[md5($chat_id)] = Array();
+        $settings = $settings_all[md5($chat_id)];
+
+        // Проверяем первый символ
+		$ch=mb_substr($text,0,1);
+		if(in_array($ch,array("/", "&", "#")))
+        {
+            list($command,$args) = explode(' ', $text, 2);
+            $args = explode(' ', $args);
+
+            // for commands like /level@enxbot
+            $command = str_replace('@enxbot', '', $command);
+
+            switch($command)
+            {
+                case '/help':
+                case '/start':
+                    apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "text" => $helptext));
+                break;
+                case '/game':
+                    switch($args[0])
+                    {
+                        case 'domain':
+                            $settings["game_domain"] = 'm.'.$args[1];
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Установлен домен $args[1]"));
+                        break;
+                        case 'id':
+                            $settings["game_id"] = $args[1];
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Установлен ID игры $args[1]"));
+                        break;
+                        case 'login':
+                            $settings["game_login"] = $args[1];
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Установлен игровой логин $args[1]"));
+                        break;
+                        case 'pass':
+                            $clear_pass = decrypt($args[1], 'Cjhjrnsczxj,tpmzyd;jgeceyekb,fyfy');
+
+                            $settings["game_pass"] = trim($clear_pass);
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Установлен пароль"));
+                        break;
+                        case 'auth':
+                            $cookies = auth($settings["game_domain"], $settings["game_login"], $settings["game_pass"]);
+                            if($cookies===false)
+                            {
+                                $result = "Не проходит авторизация на игровом движке";
+                            } else {
+                                $result = "Авторизация успешно пройдена";
+                                $settings['cookies'] = $cookies;
+                            }
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => $result));
+                        break;
+                        case 'test':
+                            $result = testGame($settings['cookies'],$settings["game_domain"],$settings["game_id"]);
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => $result ? $result : 'Ошибка'));
+                        break;
+                        case 'start':
+                            if(!in_array($message['from']['username'], $settings_all['admins']))
+                            {
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "У вас недостаточно прав для старта игры"));
+                            } else {
+                                $settings["game_chat"] = $chat_id;
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Игра привязана к чату"));
+                            }
+                        break;
+                        case 'stop':
+                            if(!in_array($message['from']['username'], $settings_all['admins']))
+                            {
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "У вас недостаточно прав для остановки бота"));
+                            } else 
+                            {
+                                $settings["game_chat"] = '';
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Игра отвязана от чата"));
+                            }
+                        break;
+                        case 'delete':
+                            if(!in_array($message['from']['username'], $settings_all['admins']))
+                            {
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "У вас недостаточно прав для остановки бота"));
+                            } else 
+                            {
+                                $settings = Array();
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Настройки игры удалены"));
+                            }
+                        break;
+                        case 'print':
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "text" => "Домен: $settings[game_domain]\nИгра $settings[game_id]\nЧат $settings[game_chat]\nЛогин $settings[game_login]\nПароль ".($settings['game_pass'] ? 'задан' : 'не задан')));
+                        break;
+                    }
+                break;
+                case '/level':
+                    if($chat_id!=$settings["game_chat"])
+                    {
+                        $result = "В данном чате бот недоступен";
+                        apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => $result));
+                    } else 
+                    {
+                        $levelText = getLevelText($settings['cookies'],$settings["game_domain"],$settings["game_id"]);
+
+                        $result = $levelText;
+                        $result_clean = $purifier->purify($result);
+                        apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => $result ? $result_clean : 'Ошибка'));
+
+                        // Ищем в тексте координаты
+                        $levelTextClean = $purifier->purify($levelText);
+                        preg_match_all('#(.*?)[\s:,;\.](-?[1-8]?\d(?:\.\d{1,6})?|90(?:\.0{1,6})?)[,]?\s+(-?(?:1[0-7]|[1-9])?\d(?:\.\d{1,6})?|180(?:\.0{1,6})?)#', $levelTextClean, $matches, PREG_SET_ORDER);
+                        foreach($matches as $match)
+                        {
+                            $text = $match[0];
+                            $lat = $match[2];
+                            $lon = $match[3];
+
+                            apiRequestJSON("sendLocation", array('chat_id' => $chat_id, "latitude" => $lat, "longitude" => $lon));
+                            apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "text" => $text));
+                        }
+                    }
+
+                break;
+                case '/encrypt':
+                    $result = encrypt($args[0], 'Cjhjrnsczxj,tpmzyd;jgeceyekb,fyfy');
+                    apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Зашифрованный пароль: $result"));
+                break;
+                case '/hints':
+                    $hints = getHints($settings['cookies'],$settings["game_domain"],$settings["game_id"]);
+                    apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "parse_mode" => 'Markdown', "text" => $hints));
+                break;
+                case '/admin':
+                    if($message['from']['username'] == 'skrashevich')
+                    {
+                        switch($args[0])
+                        {
+                            case 'add':
+                                $settings_all['admins'][] = $args[1];
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Администратор $args[1] добавлен"));
+                            break;
+                            case 'print':
+                                $result = '';
+                                foreach($settings_all['admins'] as $admin)
+                                {
+                                    $result .= "@$admin\n";
+                                }
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Список администраторов бота:\n$result"));
+                            break;
+                            case 'delete':
+                                unset($settings_all['admins'][array_search($args[1],$settings_all['admins'])]);
+                                apiRequestJSON("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => "Администратор $args[1] удален"));
+                            break;
+                        }
+                    }
+                break;
+            }
+        }
+
+        if(in_array($ch,array("&","#")))
+        {
+            if($chat_id!=$settings["game_chat"])
+            {
+                $result = "В данном чате бот недоступен";
+            } else 
+            {
+                $code=substr($text,1);
+                if(!$settings["cookies"])
+                    $cookies = auth($settings["game_domain"], $settings["game_login"], $settings["game_pass"]);
+                else {
+                    $cookies = $settings["cookies"];
+                }
+                if($cookies===false)
+                {
+                    $result = "Не проходит авторизация на игровом движке";
+                } else {
+                    $result = sendCode($cookies,$settings["game_domain"],$settings["game_id"],$code);
+                }
+            }
+            if(!$result)
+                $result = "Ошибка";
+            apiRequest("sendMessage", array('chat_id' => $chat_id, "reply_to_message_id" => $message_id, "text" => $result));
+        }
+        $settings_all[md5($chat_id)] = $settings;
+    }
+}
+
+file_put_contents('config', json_encode($settings_all));
---
+++ functions.php
@@ -0,0 +1,387 @@
+<?php
+require_once 'HTMLPurifier.standalone.php';
+$htmlpurconfig = HTMLPurifier_Config::createDefault();
+$htmlpurconfig->set('HTML', 'Allowed', '');
+$purifier = new HTMLPurifier($htmlpurconfig);
+
+function auth($domain,$login,$pass)
+{
+    $post = Array(
+        'Login' => $login,
+        'Password' => $pass,
+    );
+
+    $ch = curl_init('http://'.$domain.'/login/signin/?return=%2f');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+
+    // execute!
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+    
+    list($h1, $header, $body) = explode("\r\n\r\n", $response, 3);
+
+    $cookies = '';
+
+    $authflag = false;
+
+    $hlines = explode("\n",$header);
+    foreach($hlines as $line)
+    {
+        $line = trim($line);
+        if(strpos($line,'Set-Cookie: ')===0)
+        {
+            list($hren, $cookie) = explode(': ',$line,2);
+            list($cookie,$hren) = explode('; ',$cookie,2);
+
+            $cookies.="$cookie; ";
+            if(substr_count($cookie,'atoken'))
+            {
+                $authflag = true;
+            }
+        }
+    }
+
+    return $authflag ? $cookies : false;
+}
+
+function testGame($cookies,$domain,$gameid)
+{
+  //
+
+  $ch = curl_init('http://'.$domain.'/gameengines/encounter/play/'.$gameid);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+
+    // Вычленяем название игры
+
+    preg_match('#<a href="/games/details/'.$gameid.'/">(.*)</a>#',$response,$matches);
+
+    $levelName = $matches[1];
+
+    return $levelName;
+}
+
+function getLevelText($cookies,$domain,$gameid)
+{
+  //
+
+  $ch = curl_init('http://'.$domain.'/gameengines/encounter/play/'.$gameid);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+
+    // Вычленяем текст задания
+
+    preg_match('#<h3>Задание</h3>.*?<p>(.*?)</p>#ms',$response,$matches);
+
+    $levelText = $matches[1];
+
+    return $levelText;
+}
+
+function getHints($cookies,$domain,$gameid)
+{
+  global $purifier;
+
+  $ch = curl_init('http://'.$domain.'/gameengines/encounter/play/'.$gameid.'?lang=ru');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+
+    $hints = Array();
+
+    // Вычленяем подсказки
+
+    preg_match_all('#<span class="color_dis"><b>Подсказка&nbsp;([0-9]+)</b>&nbsp;будет через&nbsp;<span class="bold_off color_dis" id="time[0-9]*">(.*)</span><script type="text/javascript">#',$response,$matches,PREG_SET_ORDER);
+    foreach($matches as $match)
+    {
+      $hint = $match[1];
+      $remain = $match[2];
+
+      $hints[$hint] = "До открытия $remain";
+    }
+
+    preg_match_all('#<h3>Подсказка ([0-9]+)</h3>(.*?)<div class="spacer"></div>#sm',$response,$matches,PREG_SET_ORDER);
+    foreach($matches as $match)
+    {
+      $hint = $match[1];
+      $text = trim($match[2]);
+
+      $text = $purifier->purify($text);
+
+      $hints[$hint] = $text;
+    }
+
+    $result = '';
+    ksort($hints);
+    foreach($hints as $hint=>$text)
+    {
+        $result .= "*$hint*: `$text`\n";
+    }
+
+    if(!$result)
+    {
+      $result = 'Подсказок нет';
+    }
+
+    return $result;
+}
+
+function sendCode($cookies,$domain,$gameid,$code)
+{
+    global $purifier;
+    $ch = curl_init('http://'.$domain.'/gameengines/encounter/play/'.$gameid);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+
+    // Вычленяем LevelId
+    preg_match('#<input type="hidden" name="LevelId" value="(\d+)" />#',$response,$matches);
+
+    $levelId = $matches[1];
+
+    // Вычленяем LevelNumber
+    preg_match('#<input type="hidden" name="LevelNumber" value="(\d+)" />#',$response,$matches);
+
+    $levelNumber = $matches[1];
+
+
+    unset($ch,$response);
+
+    // Отправляем код
+
+    $post = Array(
+        'LevelId' => $levelId,
+        'LevelNumber' => $levelNumber,
+        'LevelAction.Answer' => $code,
+    );
+
+    $ch = curl_init('http://'.$domain.'/gameengines/encounter/play/'.$gameid.'?lang=ru');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+    // execute
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+
+    preg_match('#<span class="color_[i]?[n]?correct".*?>(.*)</span>#',$response,$matches);
+
+    $result = $purifier->purify(str_replace('&quot;','', $matches[1]));
+
+    // Если вдруг закончили игру
+    if( preg_match('#<center class="gameCongratulation">(.*)</center>#ms', $response,$matches) )
+    {
+      $result = $purifier->purify($matches[1]);
+      return $result; // Если закончили игру, то всё не имеет смысла
+    }
+
+    // Проверяем на АП
+    // Вычленяем LevelId
+    preg_match('#<input type="hidden" name="LevelId" value="(\d+)" />#',$response,$matches);
+
+    if($levelId != $matches[1])
+    {
+        $result .= "\n\nАП!";
+        return $result; // Если апнулись, то остальное не имеет смысла
+    }
+
+    // Считаем сектора
+    preg_match('#<h3>.*На уровне ([0-9]*) сектора.*?<span class="color_sec">\(осталось закрыть ([0-9]*)\)</span>#ms',$response, $matches);
+    
+    if($matches) // Если есть результаты - значит на уровне есть сектора
+    {
+      $sectors_total = $matches[1];
+      $sectors_rem = $matches[2];
+    
+      $result .= " ($sectors_rem/$sectors_total)";
+    }
+
+    return $result;
+}
+
+
+
+//
+// Telegram Bot Functions
+//
+
+function logMessage($message)
+{
+    file_put_contents('log.txt', print_r($message,true)."\n", FILE_APPEND);
+}
+
+
+function apiRequestWebhook($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  $parameters["method"] = $method;
+
+  header("Content-Type: application/json");
+  echo json_encode($parameters);
+  return true;
+}
+
+function exec_curl_request($handle) {
+  $response = curl_exec($handle);
+
+  if ($response === false) {
+    $errno = curl_errno($handle);
+    $error = curl_error($handle);
+    error_log("Curl returned error $errno: $error\n");
+    curl_close($handle);
+    return false;
+  }
+
+  $http_code = intval(curl_getinfo($handle, CURLINFO_HTTP_CODE));
+  curl_close($handle);
+
+  if ($http_code >= 500) {
+    // do not wat to DDOS server if something goes wrong
+    sleep(10);
+    return false;
+  } else if ($http_code != 200) {
+    $response = json_decode($response, true);
+    error_log("Request has failed with error {$response['error_code']}: {$response['description']}\n");
+    if ($http_code == 401) {
+      throw new Exception('Invalid access token provided');
+    }
+    return false;
+  } else {
+    $response = json_decode($response, true);
+    if (isset($response['description'])) {
+      error_log("Request was successfull: {$response['description']}\n");
+    }
+    $response = $response['result'];
+  }
+
+  return $response;
+}
+
+function apiRequest($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  foreach ($parameters as $key => &$val) {
+    // encoding to JSON array parameters, for example reply_markup
+    if (!is_numeric($val) && !is_string($val)) {
+      $val = json_encode($val);
+    }
+  }
+  $url = API_URL.$method.'?'.http_build_query($parameters);
+
+  $handle = curl_init($url);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 10);
+  curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+
+  return exec_curl_request($handle);
+}
+
+function apiRequestJson($method, $parameters) {
+  if (!is_string($method)) {
+    error_log("Method name must be a string\n");
+    return false;
+  }
+
+  if (!$parameters) {
+    $parameters = array();
+  } else if (!is_array($parameters)) {
+    error_log("Parameters must be an array\n");
+    return false;
+  }
+
+  $parameters["method"] = $method;
+
+  $handle = curl_init(API_URL);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 10);
+  curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+  curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parameters));
+  curl_setopt($handle, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+
+  return exec_curl_request($handle);
+}
+
+function sendMap($chat_id,$map=""){
+    $bot_url    = "https://api.telegram.org/bot***REMOVED-TOKEN***/";
+    $url        = $bot_url . "sendPhoto?chat_id=" . $chat_id ;
+
+    $post_fields = array('chat_id'   => $chat_id,
+        'photo'     => new CURLFile(realpath("https://static-maps.yandex.ru/1.x/?ll=37.620070,55.753630&size=450,450&z=13&l=map&pt=37.620070,55.753630,pmwtm1~37.64,55.76363,pmwtm99"))
+    );
+
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Content-Type:multipart/form-data"
+    ));
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields); 
+    $output = curl_exec($ch);
+}
+
+/**
+ * Returns an encrypted & utf8-encoded
+ */
+function encrypt($pure_string, $encryption_key) {
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $encrypted_string = mcrypt_encrypt(MCRYPT_BLOWFISH, $encryption_key, utf8_encode($pure_string), MCRYPT_MODE_ECB, $iv);
+    return base64_encode($encrypted_string);
+}
+
+/**
+ * Returns decrypted original string
+ */
+function decrypt($encrypted_string, $encryption_key) {
+    $iv_size = mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $decrypted_string = mcrypt_decrypt(MCRYPT_BLOWFISH, $encryption_key, base64_decode($encrypted_string), MCRYPT_MODE_ECB, $iv);
+    return $decrypted_string;
+}
